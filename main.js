876a732d467fd0d5e079f336ece3dc1738ba2cf7
jbchinonso <nwekejohnbosco@gmail.com>
2019-09-18 02:04:05 -0700

    I added all my files to my develop branch

---
+++ main.js
@@ -0,0 +1,34 @@
+const customertApi = "http://localhost:3000/customers";
+
+//Login Funationality
+const login = () =>{
+
+$(document).ready(function(){
+let email = (document.getElementById("email").value).trim();
+let password = (document.getElementById("password").value).trim();
+$.ajax({
+    type : "GET",
+    url : customertApi,
+    dataType: "json",
+    success: function(data){
+        data.forEach(element => {
+            console.log(element);
+            if(element.email == email && element.password == password){
+                const person ={
+                    customerId : element.id, 
+                    name : element.firstName + " " + element.lastName,
+                    debt : element.loan
+                }
+                window.localStorage.setItem('user', JSON.stringify(person));
+                window.location.replace("profile.html")
+            }
+        });
+
+        }
+    })
+   console.log(email);
+})
+
+
+}
+login();
