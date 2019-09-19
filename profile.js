89046559b6fceb419c2c40286a9504ef6958147f
jbchinonso <nwekejohnbosco@gmail.com>
2019-09-19 05:48:28 -0700

    added admin.html and update function

--- profile.js
+++ profile.js
@@ -21,10 +21,8 @@ $.ajax({
     dataType: "json",
     success: function(data){
          data.forEach(element => {
-           console.log(container);
-            console.log(element);
             container.innerHTML += 
-            `<div class = "header">$${+element.amount}</div>
+            `<div class = "header">${element.amount}</div>
             <div class= "header">${element.duration}</div><div class = "header"> ${element.status}</div><br />`
          });
     }
@@ -36,7 +34,91 @@ $.ajax({
 
 
 
+$("#req-btn").on('click', function(e){
+        e.preventDefault();
 
+   // let $firstName = $("#firstName");
+   // let $lastName = $("#lastName");
+    var $amount = $("#amount");
+    var $duration = $("#duration");
+    
+
+    const request = {
+        "amount" : $amount.val(),
+        "duration" : $duration.val(),
+        "status": "pending",
+        "customerId": `${user.customerId}`
+    }
+
+    $.ajax({
+        type: "POST",
+        url : "http://localhost:3000/requests",
+        data: request,
+        success: function(loanReq){
+            alert("Your Request has been Sent")
+        },
+        error: function(){
+            alert("Error trying to make a Request")
+        }
+
+    })
+
+
+});
+
+
+const pendingreqApi = "http://localhost:3000/Requests?status=pending";
+let $transactionbox = $('.req-transactions')
+
+$.ajax({
+    type: "GET",
+    url : pendingreqApi,
+    success: function(Reqloan){
+        Reqloan.forEach(element =>{
+            $customerId = element.customerId;
+            $.ajax({
+                type: "GET",
+                url : "http://localhost:3000/customers/"+ `${$customerId}`,
+                success: function(customer){
+                        $customer = customer['firstName'] + " " + customer['lastName']
+            $transactionbox.append( `<div class = 'wrap'>
+            <div class = 'cname'>${$customer} </div>
+            <div class = 'content'>${element.amount}</div>
+            <div class = 'content'>${element.duration}</div>
+            <div class = 'content'><button data-id = ${element.id} id = "Approvebtn" class = "replybtn">Approve </button></div>
+            <div class = 'content'><button data-id = ${element.id} id = 'Denybtn' class = "replybtn"> Deny </button></div>
+            <div class = 'content'><button data-id = ${element.id} id = 'Deletebtn' class = "replybtn">Delete </button></div>
+            
+            </div> <br />`)
+
+            
+            
+
+                    }
+        })
+    })
+    },
+    
+})
+
+
+$transactionbox .delegate('#Approvebtn','click', function (){
+    console.log('yeah');
+    var data = {
+        "status": 'Approved'
+    }
+    $.ajax({
+        type: "PATCH",
+        url : "http://localhost:3000/requests/" + $(this).attr('data-id'),
+        data: data,
+        success: function(){
+            alert('The Loan has been Approved')
+        },
+        error: function(){
+            alert("Approval Failed")
+        }
+    })
+})
 
 
 })
